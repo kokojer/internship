78f54f5a7c075aafd37734345b3a888bf678e591
kokojer <gordei80z34@gmail.com>
2022-05-15 12:35:28 +0300

    add readme

--- src/modules/main/activity/user-info/user-info.jsx
+++ src/modules/main/activity/user-info/user-info.jsx
@@ -6,7 +6,7 @@ export const UserInfo = () => {
 
     return (
         <Row>
-            <Col xs={12} sm={5} md={4} lg={12} className='px-2 px-lg-3 mb-2 mb-sm-0'>
+            <Col xs={12} sm={5} md={4} lg={12} className='px-2 px-lg-3 mb-2 mb-sm-0 mb-lg-3 mb-xxl-4 '>
                 <Userimg />
             </Col>
             <Col xs={12} sm={7} md={8} lg={12} className='px-2 px-lg-3'>
--- src/modules/main/activity/user-info/userImg/userImg.jsx
+++ src/modules/main/activity/user-info/userImg/userImg.jsx
@@ -3,7 +3,7 @@ import {VkIcon} from "../../../../icons/VkIcon";
 export const Userimg = () => {
 
     return (
-        <div className={`d-flex flex-column align-items-center py-3 py-xl-4 mb-0 mb-lg-3 mb-xxl-4 bg-white ${s.userImg}`} style={{border: '1px solid #DDE0E6', borderRadius:'3px'}}>
+        <div className={`d-flex flex-column align-items-center py-3 py-xl-4 bg-white ${s.userImg}`} style={{border: '1px solid #DDE0E6', borderRadius:'3px'}}>
             <div className={`d-flex align-items-center justify-content-center mb-3 ${s.imgContainer}`}></div>
             <h3 style={{fontSize:'18px'}} className='mb-2'>Harold Pain</h3>
             <p style={{fontSize:'14px', color: "#A1A5AE"}} className='pb-3 m-0'>UX/UI designer</p>
